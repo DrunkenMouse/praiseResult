e4080f2d8dae0f561e2b0c10255eb6b23a0f18d8
残月 <632342527@qq.com>
2016-12-07 15:54:12 +0800

    Initial Commit

---
+++ 点赞效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  点赞效果
+//
+//  Created by 王奥东 on 16/12/7.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 点赞效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  点赞效果
+//
+//  Created by 王奥东 on 16/12/7.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
